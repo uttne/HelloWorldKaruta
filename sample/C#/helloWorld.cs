2c97bc358ab0b81c40a6b1b9e7fd26b327a349ec
龍矢 瀧上 <uttne@outlook.com>
2017-08-03 23:53:20 +0900

    Add sample source.

---
+++ sample/C#/helloWorld.cs
@@ -0,0 +1,10 @@
+namespace HelloWrld
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Hello, World!");
+        }
+    }
+}
